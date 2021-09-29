9e363c7bfa9b00abafb2b34d40be188d13fa6dc0
Daniel Calvin <arockne2003@yahoo.com>
2021-09-29 14:21:30 -0700

    commit tree 4ab9a946d5687bd902a5fd7eefb4b2f8c0d92e1f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length > 1
+    array[-1] = "and #{array[-1]}"
+  end
+
+  case array.length
+  when 1
+    array.join()
+  when 2
+    array.join(' ')
+  else
+    array.join(', ')
+  end
 end
